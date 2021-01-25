36034d644c34b4ecf2008df6624c59a2d49bd0ec
phwt.smwt <phwt.smwt@gmail.com>
2021-01-25 20:32:36 +0700

    Setup and Teardown

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -1,4 +1,16 @@
 describe(`The Question List Component`, () => {
+  beforeEach(() => {
+    console.log("beforeEach");
+  });
+  beforeAll(() => {
+    console.log("beforeAll");
+  });
+  afterEach(() => {
+    console.log("afterEach");
+  });
+  afterAll(() => {
+    console.log("afterAll");
+  });
   it(`Should display a list of items`, () => {
     expect(1 + 1).toEqual(2);
   });
