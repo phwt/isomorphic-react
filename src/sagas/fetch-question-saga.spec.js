8eab9d10db0e6d67e580995024ab50d393aea111
phwt.smwt <phwt.smwt@gmail.com>
2021-01-25 22:00:15 +0700

    Mock (broken)

---
+++ src/sagas/fetch-question-saga.spec.js
@@ -0,0 +1,7 @@
+import { handleFetchQuestion } from "./fetch-question-saga";
+
+describe("Fetch Question Saga", () => {
+  it("should fetch the questions", () => {
+    console.log("Test running");
+  });
+});
